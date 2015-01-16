c7794905aea17e8270c33baa27883adc37203cba
Corey Z <czuares@gmail.com>
2015-01-16 15:57:36 -0500

    fun stuff added!

--- app/scripts/angular/pushPresenceApp.js
+++ app/scripts/angular/pushPresenceApp.js
@@ -1,211 +1,222 @@
 'use strict';
 
-var pushPresenceApp = angular.module('pushPresenceApp',  ['ui.bootstrap','frapontillo.bootstrap-switch']);
+var pushPresenceApp = angular.module('pushPresenceApp', ['ui.bootstrap', 'frapontillo.bootstrap-switch']);
 
 pushPresenceApp.controller('OptionsCtrl', ['$scope', '$window',
   function ($scope, $window) {
 
-    $scope.deviceId = null;
-    $scope.online = navigator.onLine;
-    $scope.DaysOfWeek = $window.DaysOfWeek;
+        $scope.deviceId = null;
+        $scope.online = navigator.onLine;
+        $scope.DaysOfWeek = $window.DaysOfWeek;
 
-    $scope.model = new DataModel();
-    $scope.config = new ConfigurationModel();
+        $scope.model = new DataModel();
+        $scope.config = new ConfigurationModel();
 
-    var init = function(){
-      chrome.storage.sync.get($scope.config, function(items){
-        console.log('Binding config from synced storage');
-        $scope.config = items;
-        loadApiKey();
+        var init = function () {
+            chrome.storage.sync.get($scope.config, function (items) {
+                console.log('Binding config from synced storage');
+                $scope.config = items;
+                loadApiKey();
 
-        $scope.$apply();
-      });
+                $scope.$apply();
+            });
 
-      chrome.storage.local.get($scope.model, function(items){
-        console.log('Binding data model from local storage');
-        $scope.model = items;
+            chrome.storage.local.get($scope.model, function (items) {
+                console.log('Binding data model from local storage');
+                $scope.model = items;
 
-        $scope.$apply();
-      });
-    }
-
-    var onlineStateChanged = function(online){
-      $scope.$apply(function() {
-        console.log((online ? 'on' : 'off' ) + 'line at ' + new Date());
-        $scope.online = online;
-      });
-    }
-
-    var loadApiKey = function(){
-      PushBullet.APIKey = $scope.config.pushBulletApiToken;
-      console.log('api key set');
-    };
-
-    var getDevices = function(){
-      console.log('Populating devices');
-      PushBullet.devices(function(err, res){
-        if(err){
-          console.log(err);
-          return;
+                $scope.$apply();
+            });
         }
 
-        console.log(res.devices);
-        $scope.config.devices = _.chain(res.devices)
-        .where({active:true}).map(function(data){
-          return _.pick(data,['iden','nickname']);
-        }).value();
+        var onlineStateChanged = function (online) {
+            $scope.$apply(function () {
+                console.log((online ? 'on' : 'off') + 'line at ' + new Date());
+                $scope.online = online;
+            });
+        }
 
-        $scope.$apply();
-      });
-    };
+        var loadApiKey = function () {
+            PushBullet.APIKey = $scope.config.pushBulletApiToken;
+            console.log('api key set');
+        };
 
-    var resetModel = function(){
-      chrome.storage.local.clear(function(){
-        console.log('Reset local');
-        $scope.model = new DataModel();
-        $scope.$apply();
-      });
-    };
+        var getDevices = function () {
+            console.log('Populating devices');
+            PushBullet.devices(function (err, res) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
 
-    var resetConfig = function(){
-      chrome.storage.sync.clear(function(){
-        console.log('Reset synced');
-        $scope.config = new ConfigurationModel();
-        $scope.$apply();
-      });
-    }
+                console.log(res.devices);
+                $scope.config.devices = _.chain(res.devices)
+                    .where({
+                        active: true
+                    }).map(function (data) {
+                        return _.pick(data, ['iden', 'nickname']);
+                    }).value();
 
-    $window.addEventListener("offline", function () {
-      onlineStateChanged(false);
-    }, false);
-    $window.addEventListener("online", function () {
-      onlineStateChanged(true);
-    }, false);
+                $scope.$apply();
+            });
+        };
 
-    chrome.storage.onChanged.addListener(function (changes, areaName) {
-      console.log(areaName + ' data changed - reloading');
-      init();
-    });
+        var resetModel = function () {
+            chrome.storage.local.clear(function () {
+                console.log('Reset local');
+                $scope.model = new DataModel();
+                $scope.$apply();
+            });
+        };
 
-    init();
+        var resetConfig = function () {
+            chrome.storage.sync.clear(function () {
+                console.log('Reset synced');
+                $scope.config = new ConfigurationModel();
+                $scope.$apply();
+            });
+        }
 
-    $scope.ApiKeySet = function(){
-      return $scope.config.pushBulletApiToken.length == 32;
-    };
+        $window.addEventListener("offline", function () {
+            onlineStateChanged(false);
+        }, false);
+        $window.addEventListener("online", function () {
+            onlineStateChanged(true);
+        }, false);
 
-    $scope.SetToken = function(){
-      loadApiKey();
-
-      if(PushBullet.APIKey.length == 32){
-        getDevices();
-      } else if (PushBullet.APIKey == ''){
-        resetConfig();
-        resetModel();
-      }
-    }
-
-    $scope.GetDeviceName = function(deviceId){
-      var name = _.findWhere($scope.config.devices, 
-        {iden : deviceId }).nickname;
-      return (!!name) ? name : deviceId;
-    }
-
-    $scope.DeviceSelected = function(){
-      console.log('Selected device ' + $scope.deviceId);
-    };
-
-    $scope.Save = function(){
-      chrome.storage.sync.set($scope.config, function(){
-        $scope.saveStatus = 'Saved';
-        console.log('Saved config');
-        $scope.$apply();
-      });
-
-      chrome.storage.local.set($scope.model, function(){
-        $scope.saveStatus = 'Saved';
-        console.log('Saved data model');
-        $scope.$apply();
-      });
-    };
-
-    $scope.Reset = function(){
-      resetModel();
-    };
-
-    $scope.RefreshDevices= function(){
-      $scope.deviceId = null;
-      getDevices();
-    };
-
-    $scope.AddSubscription = function(){
-      var sub = new Subscription();
-      sub.deviceId = $scope.deviceId;
-      $scope.model.subscriptions.push(sub);
-    };
-
-    $scope.TestPush = function(){
-      var res = PushBullet.push('note',
-        $scope.deviceId, null, {
-          title : 'Test from PushPresence',
-          body : 'Test Notification'
+        chrome.storage.onChanged.addListener(function (changes, areaName) {
+            console.log(areaName + ' data changed - reloading');
+            init();
         });
-      console.log(res);
-    };
+
+        init();
+
+        $scope.ApiKeySet = function () {
+            return $scope.config.pushBulletApiToken.length == 32;
+        };
+
+        $scope.SetToken = function () {
+            loadApiKey();
+
+            if (PushBullet.APIKey.length == 32) {
+                getDevices();
+            } else if (PushBullet.APIKey == '') {
+                resetConfig();
+                resetModel();
+            }
+        }
+
+        $scope.GetDeviceName = function (deviceId) {
+            var name = _.findWhere($scope.config.devices, {
+                iden: deviceId
+            }).nickname;
+            return (!!name) ? name : deviceId;
+        }
+
+        $scope.DeviceSelected = function () {
+            console.log('Selected device ' + $scope.deviceId);
+        };
+
+        $scope.Save = function () {
+            chrome.storage.sync.set($scope.config, function () {
+                $scope.saveStatus = 'Saved';
+                console.log('Saved config');
+                $scope.$apply();
+            });
+
+            chrome.storage.local.set($scope.model, function () {
+                $scope.saveStatus = 'Saved';
+                console.log('Saved data model');
+                $scope.$apply();
+            });
+        };
+
+        $scope.Reset = function () {
+            resetModel();
+        };
+
+        $scope.RefreshDevices = function () {
+            $scope.deviceId = null;
+            getDevices();
+        };
+
+        $scope.AddSubscription = function () {
+            var sub = new Subscription();
+            sub.deviceId = $scope.deviceId;
+            $scope.model.subscriptions.push(sub);
+        };
+
+        $scope.TestPush = function () {
+            var res = PushBullet.push('note',
+                $scope.deviceId, null, {
+                    title: 'Test from PushPresence',
+                    body: 'Test Notification'
+                });
+            console.log(res);
+        };
 
   }]);
 
-pushPresenceApp.directive('isodatestring', function() {
-  return {
-    //hack to handle chrome storage not persisting date objects
-    //https://stackoverflow.com/a/12947995
-    restrict: 'A',
-    require: 'ngModel',
-    link: function(scope, element, attr, ngModel) {
-      ngModel.$parsers.push(function(date){
-        return new Date(date).toISOString();
-      });
-    }
-  };
-});
-
-pushPresenceApp.directive('appSubscription',  function() {
-  return {
-    restrict: 'E',
-    scope:{
-      model: "=data"
-    },
-    link: function($scope, $parent, element, attrs) {
-      $scope.AddTimeframe = function() {
-        $scope.model.timeframes.push(new Timeframe());
-      };
-
-      $scope.RemoveTimeFrame = function(idx) {
-        console.log('index: ' + idx);
-        $scope.model.timeframes.splice(idx, 1);
-      };
-
-      $scope.DayName = function(day){
-        return $scope.$parent.DaysOfWeek[day.id];
-      };
-
-      $scope.GetPlaceHolderText = function(evt){
-        var title = capitalize(evt.eventType);
-        if(evt.subscribed){
-          return title;
-        } else{
-          return title + " (Enable event to customize message)";
+pushPresenceApp.directive('isodatestring', function () {
+    return {
+        //hack to handle chrome storage not persisting date objects
+        //https://stackoverflow.com/a/12947995
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModel) {
+            ngModel.$parsers.push(function (date) {
+                return new Date(date).toISOString();
+            });
         }
-      };
-    },
-    templateUrl: '../templates/subscription.html'
-  };
+    };
 });
 
-pushPresenceApp.filter('capitalize', function() {
-  return capitalize;
+pushPresenceApp.directive('appSubscription', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            model: "=data"
+        },
+        link: function ($scope, $parent, element, attrs) {
+            $scope.AddTimeframe = function () {
+                $scope.model.timeframes.push(new Timeframe());
+            };
+
+            $scope.RemoveTimeFrame = function (idx) {
+                console.log('index: ' + idx);
+                $scope.model.timeframes.splice(idx, 1);
+            };
+
+            $scope.DayName = function (day) {
+                return $scope.$parent.DaysOfWeek[day.id];
+            };
+
+            $scope.GetScheduleName = function (timeframe) {
+                if (timeframe.allDay) {
+                    return "All Day Every Day";
+                }
+
+                return "Custom Schedule";
+            };
+
+            $scope.GetPlaceHolderText = function (evt) {
+                var title = capitalize(evt.eventType);
+                if (evt.subscribed) {
+                    return title;
+                } else {
+                    return title + " (Enable event to customize message)";
+                }
+            };
+        },
+        templateUrl: '../templates/subscription.html'
+    };
 });
 
-var capitalize = function(input, all) {
+pushPresenceApp.filter('capitalize', function () {
+    return capitalize;
+});
+
+var capitalize = function (input, all) {
     //uses prototype method found in datamodel.js
     return (!!input) ? input.capitalize() : '';
-  };
+};
--- app/scripts/datamodel.js
+++ app/scripts/datamodel.js
@@ -1,58 +1,68 @@
 'use strict';
 
-var EventTypes = [ 'active','locked','idle' ];
-var DaysOfWeek = {  0: 'Sun', 1: 'Mon', 2: 'Tues', 
-3: 'Weds', 4: 'Thurs', 5: 'Fri', 6: 'Sat' };
+var EventTypes = ['active', 'locked', 'idle'];
+var DaysOfWeek = {
+    0: 'Sun',
+    1: 'Mon',
+    2: 'Tues',
+    3: 'Weds',
+    4: 'Thurs',
+    5: 'Fri',
+    6: 'Sat'
+};
 
-if(Object.freeze){
-  Object.freeze(EventTypes);
-  Object.freeze(DaysOfWeek);
+if (Object.freeze) {
+    Object.freeze(EventTypes);
+    Object.freeze(DaysOfWeek);
 }
 
 function ConfigurationModel() {
-  this.pushBulletApiToken = '';
-  this.devices = [];
+    this.pushBulletApiToken = '';
+    this.devices = [];
 }
 
-function DataModel(){
-  this.subscriptions = [];
-  this.globalEnabled = true;
+function DataModel() {
+    this.subscriptions = [];
+
+    this.globalEnabled = true;
+    this.showDesktopNotifications = true;
 }
 
 function Subscription() {
-  this.deviceId = null;
-  this.timeframes = [new Timeframe()];
+    this.deviceId = null;
+    this.enabled = true;
+    this.timeframes = [new Timeframe()];
 }
 
-function Event(et){
-  this.eventType = et;
-  this.subscribed = false;
-  this.customMessage = '';
+function Event(et) {
+    this.eventType = et;
+    this.subscribed = false;
+    this.customMessage = '';
 }
 
-function Day(dayIndex){
-  this.id = dayIndex;
-  this.enabled = false;
+function Day(dayIndex) {
+    this.id = dayIndex;
+    this.enabled = false;
 }
 
 function Timeframe() {
-  this.name = '';
-  this.allDay = true;
-  this.begin = new Date().toISOString();
-  this.end = new Date().toISOString();
-  this.invert = false;
+    this.name = '';
+    this.allDay = true;
+    this.begin = new Date().toISOString();
+    this.end = new Date().toISOString();
+    this.invert = false;
 
-  this.days = [];
-  for(var d in DaysOfWeek){
-    this.days.push(new Day(d));
-  }
+    this.days = [];
+    for (var d in DaysOfWeek) {
+        this.days.push(new Day(d));
+    }
 
-  this.events = [];
-  for(var e in EventTypes){
-    this.events.push(new Event(EventTypes[e]));
-  }
+    this.events = [];
+    for (var e in EventTypes) {
+        this.events.push(new Event(EventTypes[e]));
+    }
 }
 
 String.prototype.capitalize = function () {
-  return this.charAt(0).toUpperCase() + this.slice(1);
+    return this.charAt(0).toUpperCase() + this.slice(1);
 };

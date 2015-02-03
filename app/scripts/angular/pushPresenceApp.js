88c34c6c60a6dd258e2a913ce884bf0f7288b17d
Corey Z <corey@intangibleobject.com>
2015-02-03 07:15:32 -0500

    style cleanup
    created alert showing current state (online/offline/enabled/disabled)
    
    updates for editable tab names

--- app/scripts/angular/pushPresenceApp.js
+++ app/scripts/angular/pushPresenceApp.js
@@ -3,76 +3,76 @@
 var pushPresenceApp = angular.module('pushPresenceApp', ['ui.bootstrap', 'frapontillo.bootstrap-switch']);
 
 pushPresenceApp.controller('OptionsCtrl', ['$scope', '$window',
-  function($scope, $window) {
+function($scope, $window) {
 
-    $scope.debug = false;
-    $scope.online = navigator.onLine;
-    $scope.DaysOfWeek = $window.DaysOfWeek;
+  $scope.debug = false;
+  $scope.online = navigator.onLine;
+  $scope.DaysOfWeek = $window.DaysOfWeek;
 
-    var initModel = new DataModel();
-    $scope.model = angular.copy(initModel);
-    $scope.config = new ConfigurationModel();
+  var initModel = new DataModel();
+  $scope.model = angular.copy(initModel);
+  $scope.config = new ConfigurationModel();
 
-    var init = function() {
-      chrome.storage.sync.get($scope.config, function(items) {
-        console.log('Binding config from synced storage');
-        $scope.config = items;
-        loadApiKey();
+  var init = function() {
+    chrome.storage.sync.get($scope.config, function(items) {
+      console.log('Binding config from synced storage');
+      $scope.config = items;
+      loadApiKey();
 
-        $scope.$apply();
+      $scope.$apply();
+    });
+
+    chrome.storage.local.get($scope.model, function(items) {
+      console.log('Binding data model from local storage');
+      $scope.model = items;
+
+      $scope.$apply();
+    });
+  };
+
+  var onlineStateChanged = function(online) {
+    $scope.$apply(function() {
+      console.log((online ? 'on' : 'off') + 'line at ' + new Date());
+      $scope.online = online;
+    });
+  };
+
+  var loadApiKey = function() {
+    PushBullet.APIKey = $scope.config.pushBulletApiToken;
+    console.log('API key set');
+  };
+
+  var getDevices = function() {
+    console.log('Populating devices');
+    PushBullet.devices(function(err, res) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      var activeDevices = _.where(res.devices, {
+        active: true
       });
+      console.log('activeDevices', activeDevices);
 
-      chrome.storage.local.get($scope.model, function(items) {
-        console.log('Binding data model from local storage');
-        $scope.model = items;
-
-        $scope.$apply();
-      });
-    };
-
-    var onlineStateChanged = function(online) {
-      $scope.$apply(function() {
-        console.log((online ? 'on' : 'off') + 'line at ' + new Date());
-        $scope.online = online;
-      });
-    };
-
-    var loadApiKey = function() {
-      PushBullet.APIKey = $scope.config.pushBulletApiToken;
-      console.log('API key set');
-    };
-
-    var getDevices = function() {
-      console.log('Populating devices');
-      PushBullet.devices(function(err, res) {
-        if (err) {
-          console.log(err);
-          return;
-        }
-
-        var activeDevices = _.where(res.devices, {
-          active: true
-        });
-        console.log('activeDevices', activeDevices);
-
-        var validSubscriptions = _.filter($scope.model.subscriptions,
-          function(sub) {
-            //remove subs with devices missing/deactivated in PushBullet
-            return _.where(activeDevices, {
-              iden: sub.device.id
-            });
+      var validSubscriptions = _.filter($scope.model.subscriptions,
+        function(sub) {
+          //remove subs with devices missing/deactivated in PushBullet
+          return _.where(activeDevices, {
+            iden: sub.device.id
           });
+        });
         console.log('validSubscriptions', validSubscriptions);
 
         var newSubs = _.chain(activeDevices)
-          .filter(function(dev) {
-            //filter out existing
-            return !_.some(validSubscriptions, function(sub) {
-              return sub.device.id == dev.iden;
-            });
-          }).map(function(data) {
-            return new Subscription(data);
-          }).value();
+        .filter(function(dev) {
+          //filter out existing
+          return !_.some(validSubscriptions, function(sub) {
+            return sub.device.id == dev.iden;
+          });
+        }).map(function(data) {
+          return new Subscription(data);
+        }).value();
         console.log('newSubs', newSubs);
 
         $scope.model.subscriptions = _.union(validSubscriptions, newSubs);
@@ -83,7 +83,7 @@ pushPresenceApp.controller('OptionsCtrl', ['$scope', '$window',
 
     var resetData = function() {
       if (!window.confirm("Warning!\n\nAll of your saved data will be erased. Are you sure you want to continue?"))
-        return;
+      return;
 
       console.log('Resetting models');
       $scope.model = new DataModel();
@@ -150,7 +150,7 @@ pushPresenceApp.controller('OptionsCtrl', ['$scope', '$window',
       }
 
       if (!window.confirm("This device will be removed. To re-add it, please refresh devices.\n\nWould you like to continue?"))
-        return false;
+      return false;
 
       $scope.model.subscriptions = _.without($scope.model.subscriptions, sub);
     };
@@ -180,7 +180,7 @@ pushPresenceApp.controller('OptionsCtrl', ['$scope', '$window',
 
     $scope.SetToken = function() {
       if (!$scope.ApiKeySet())
-        return;
+      return;
 
       loadApiKey();
       getDevices();
@@ -194,129 +194,129 @@ pushPresenceApp.controller('OptionsCtrl', ['$scope', '$window',
       switch (sub.device.type) {
         case 'chrome':
           return 'globe';
-        default:
-          return 'mobile';
-          // case 'ios':
-          // case 'iphone':
-          // case 'android':
-          //   break;
+          default:
+            return 'mobile';
+            // case 'ios':
+            // case 'iphone':
+            // case 'android':
+            //   break;
+          }
+        };
+
+        $scope.GetDeviceName = function(sub) {
+          if (!sub) return 'Unknown';
+          return sub.device.name;
+        };
+
+        $scope.DeviceSelected = function() {
+          console.log('Selected device ' + $scope.deviceId);
+        };
+
+        $scope.RefreshDevices = function() {
+          if (!window.confirm("New devices will be added and stale devices will be removed.\n\nWould you like to continue?"))
+          return;
+          getDevices();
+        };
+
       }
-    };
+      ]);
 
-    $scope.GetDeviceName = function(sub) {
-      if (!sub) return 'Unknown';
-      return sub.device.name;
-    };
-
-    $scope.DeviceSelected = function() {
-      console.log('Selected device ' + $scope.deviceId);
-    };
-
-    $scope.RefreshDevices = function() {
-      if (!window.confirm("New devices will be added and stale devices will be removed.\n\nWould you like to continue?"))
-        return;
-      getDevices();
-    };
-
-  }
-]);
-
-pushPresenceApp.directive('isodatestring', function() {
-  return {
-    //hack to handle chrome storage not persisting date objects
-    //https://stackoverflow.com/a/12947995
-    restrict: 'A',
-    require: 'ngModel',
-    link: function(scope, element, attr, ngModel) {
-      ngModel.$parsers.push(function(date) {
-        return new Date(date).toISOString();
+      pushPresenceApp.directive('isodatestring', function() {
+        return {
+          //hack to handle chrome storage not persisting date objects
+          //https://stackoverflow.com/a/12947995
+          restrict: 'A',
+          require: 'ngModel',
+          link: function(scope, element, attr, ngModel) {
+            ngModel.$parsers.push(function(date) {
+              return new Date(date).toISOString();
+            });
+          }
+        };
       });
-    }
-  };
-});
 
-pushPresenceApp.directive('appSubscription', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      model: "=data"
-    },
-    link: function($scope, $parent, element, attrs) {
-      $scope.AddTimeframe = function() {
-        $scope.model.timeframes.push(new Timeframe());
-      };
+      pushPresenceApp.directive('appSubscription', function() {
+        return {
+          restrict: 'E',
+          scope: {
+            model: "=data"
+          },
+          link: function($scope, $parent, element, attrs) {
+            $scope.AddTimeframe = function() {
+              $scope.model.timeframes.push(new Timeframe());
+            };
 
-      $scope.RemoveTimeFrame = function(idx, e) {
-        if (e) {
-          e.preventDefault();
-          e.stopPropagation();
-        }
-        console.log('index: ' + idx);
-        $scope.model.timeframes.splice(idx, 1);
-      };
+            $scope.RemoveTimeFrame = function(idx, e) {
+              if (e) {
+                e.preventDefault();
+                e.stopPropagation();
+              }
+              console.log('index: ' + idx);
+              $scope.model.timeframes.splice(idx, 1);
+            };
 
-      $scope.DayName = function(day) {
-        return $scope.$parent.DaysOfWeek[day.id];
-      };
+            $scope.DayName = function(day) {
+              return $scope.$parent.DaysOfWeek[day.id];
+            };
 
-      $scope.GetScheduleName = function(timeframe) {
-        if (timeframe.allDay) {
-          return "All Day Every Day";
-        }
-        return "Custom Schedule";
-      };
+            $scope.GetScheduleName = function(timeframe) {
+              if (timeframe.allDay) {
+                return "All Day Every Day";
+              }
+              return "Custom Schedule";
+            };
 
-      $scope.TestPush = function(deviceId) {
-        if (!deviceId) return;
+            $scope.TestPush = function(deviceId) {
+              if (!deviceId) return;
 
-        var res = PushBullet.push('note', deviceId, null, {
-          title: 'Test from PushPresence',
-          body: 'Test Notification'
-        });
-        console.log(res);
-      };
+              var res = PushBullet.push('note', deviceId, null, {
+                title: 'Test from PushPresence',
+                body: 'Test Notification'
+              });
+              console.log(res);
+            };
 
-      // $scope.OnAllDayChanged = function(idx) {
-      //   console.log('Timeframe changed');
-      //   var timeframe = $scope.model.timeframes[idx];
-      //   if (timeframe.allDay) {
-      //     $scope.model.timeframes[idx] = new Timeframe();
-      //   }
-      // };
+            // $scope.OnAllDayChanged = function(idx) {
+            //   console.log('Timeframe changed');
+            //   var timeframe = $scope.model.timeframes[idx];
+            //   if (timeframe.allDay) {
+            //     $scope.model.timeframes[idx] = new Timeframe();
+            //   }
+            // };
 
-      $scope.GetPlaceHolderText = function(evt) {
-        var title = capitalize(evt.eventType);
-        if (evt.subscribed) {
-          return title;
-        } else {
-          return title + " (Enable event to customize message)";
-        }
-      };
-    },
-    templateUrl: '../templates/subscription.html'
-  };
-});
-
-pushPresenceApp.directive('focusMe', function($timeout) {
-  return {
-    scope: { trigger: '@focusMe' },
-    link: function(scope, element) {
-      scope.$watch('trigger', function(value) {
-        if(value === "true") {
-          $timeout(function() {
-            element[0].focus();
-          });
-        }
+            $scope.GetPlaceHolderText = function(evt) {
+              var title = capitalize(evt.eventType);
+              if (evt.subscribed) {
+                return title;
+              } else {
+                return title + " (Enable event to customize message)";
+              }
+            };
+          },
+          templateUrl: '../templates/subscription.html'
+        };
       });
-    }
-  };
-});
 
-pushPresenceApp.filter('capitalize', function() {
-  return capitalize;
-});
+      pushPresenceApp.directive('focusMe', function($timeout) {
+        return {
+          scope: { trigger: '@focusMe' },
+          link: function(scope, element) {
+            scope.$watch('trigger', function(value) {
+              if(value === "true") {
+                $timeout(function() {
+                  element[0].focus();
+                });
+              }
+            });
+          }
+        };
+      });
 
-var capitalize = function(input, all) {
-  //uses prototype method found in datamodel.js
-  return (!!input) ? input.capitalize() : '';
-};
+      pushPresenceApp.filter('capitalize', function() {
+        return capitalize;
+      });
+
+      var capitalize = function(input, all) {
+        //uses prototype method found in datamodel.js
+        return (!!input) ? input.capitalize() : '';
+      };
